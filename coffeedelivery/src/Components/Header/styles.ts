d398844db7d56977839dadbde9f77ca47693238b
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-12 16:08:09 -0300

    Start layout

---
+++ coffeedelivery/src/Components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderConteiner = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 2rem 10rem;
+    gap: 51rem;
+
+    position: fixed;
+    height: 6.5rem;
+`;
