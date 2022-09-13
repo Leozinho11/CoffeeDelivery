b4266a2d5a0c736fbcf2be2114da2da6e5664b75
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-13 18:49:55 -0300

    updated

--- coffeedelivery/src/Components/Cards/styles.ts
+++ coffeedelivery/src/Components/Cards/styles.ts
@@ -72,3 +72,78 @@ export const CardSubtitle = styled.p`
 
     color: ${props => props.theme["gray-500"]};
 `;
+
+export const PriceContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+
+    height: 2rem;
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+    margin-top: 2rem;
+`;
+
+export const PriceText = styled.p`
+    width: 4rem;
+    height: 2rem;
+
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1rem;
+    text-align: right;
+    white-space: nowrap;
+    color: ${props => props.theme["gray-600"]};
+
+    span{
+        font-family: 'Baloo 2', cursive;
+        font-weight: 800;
+        font-size: 1.5rem;
+    }
+`;
+
+export const Counter = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    gap: 0.25rem;
+
+    width: 4.5rem;
+    height: 2.5rem;
+
+    border-radius: 6px;
+    background: ${props => props.theme["gray-300"]};
+
+    button{
+        border: none;
+        background: ${props => props.theme["gray-300"]};
+
+        width: 1rem;
+        height: 1rem;
+        
+        color: ${props => props.theme["purple-500"]};
+
+        &:hover{
+            color: ${props => props.theme["purple-700"]};
+        }
+    }
+`;
+
+export const CartButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    margin-left: 0.5rem;
+
+    background-color: ${props => props.theme["purple-700"]};
+    color: ${props => props.theme.white};
+
+    border: none;
+    border-radius: 6px;
+`;
