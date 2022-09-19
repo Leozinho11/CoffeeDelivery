77c3da75cad7a1ad91482c5f14a58eeb5f3e91fa
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-18 22:34:11 -0300

    Update Checkout

--- coffeedelivery/src/pages/Checkout/Index.tsx
+++ coffeedelivery/src/pages/Checkout/Index.tsx
@@ -1,5 +1,5 @@
-import { MapPinLine } from "phosphor-react";
-import { AddressForm, CheckoutContainer, CheckoutForm, CompleteFormText, FormInput } from "./styles";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+import { AddressForm, CheckoutContainer, CheckoutForm, CompleteFormText, FormInput, PaymentContainer, PaymentWay, PaymentWayContainer } from "./styles";
 
 export function Checkout (){
     return(
@@ -15,16 +15,29 @@ export function Checkout (){
                         </section>
 
                         <FormInput>
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
+                            <input type="text" className="CEP" placeholder="CEP"/>
+                            <input type="text" className="Rua" placeholder="Rua"/>
+                            <input type="text" className="Número" placeholder="Número"/>
+                            <input type="text" className="Complemento" placeholder="Complemento"/>
+                            <input type="text" className="Bairro" placeholder="Bairro"/>
+                            <input type="text" className="Cidade" placeholder="Cidade"/>
+                            <input type="text" className="UF" placeholder="UF"/>
                         </FormInput>
-                        
                     </AddressForm>
+
+                    <PaymentContainer>
+                        <section>
+                            <CurrencyDollar size={22}/><header>Pagamento
+                                <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+                            </header>
+                        </section>
+
+                        <PaymentWayContainer>
+                            <PaymentWay><CreditCard size={16}/>Cartão de Crédito</PaymentWay>
+                            <PaymentWay><Bank size={16}/>Cartão de Débito</PaymentWay>
+                            <PaymentWay><Money size={16}/>Dinheiro</PaymentWay>
+                        </PaymentWayContainer>
+                    </PaymentContainer>
                 </form>
             </CheckoutForm>
         </CheckoutContainer>
