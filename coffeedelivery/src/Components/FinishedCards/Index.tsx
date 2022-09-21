b6918f8faf4d9682efdd64f66a55a6e30733c03a
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-20 21:30:57 -0300

    Checkout card done

--- coffeedelivery/src/Components/FinishedCards/Index.tsx
+++ coffeedelivery/src/Components/FinishedCards/Index.tsx
@@ -1,28 +1,36 @@
-import { CheckoutCardContainer, CheckoutCardCounter, CheckoutCardInfo } from "./styles";
+import { CheckoutCardButtonsContainer, CheckoutCardContainer, CheckoutCardCounter, CheckoutCardInfo, CheckoutCardInfoContainer, CheckoutCardPrice, CheckoutCardRemoveButton, CheckoutCardText } from "./styles";
 import Expresso from '../../images/Expresso.svg';
-import { Minus, Plus } from "phosphor-react";
+import { Minus, Plus, Trash } from "phosphor-react";
 
 export function CheckoutCard(){
     return(
         <CheckoutCardContainer>
 
-            <CheckoutCardInfo>
+            <CheckoutCardInfoContainer>
              <img src={Expresso} alt="" />
-            
-            <CheckoutCardCounter>
-            <button>
-                <Minus />
-            </button>
+            <CheckoutCardInfo>
+            <CheckoutCardText>Expresso Tradicional</CheckoutCardText>
+                <CheckoutCardButtonsContainer>
+                    <CheckoutCardCounter>
+                    <button>
+                        <Minus />
+                    </button>
 
-            <span>1</span>
+                    <span>1</span>
 
-            <button>
-                <Plus />
-            </button>
-            </CheckoutCardCounter>
-            
+                    <button>
+                        <Plus />
+                    </button>
+                    </CheckoutCardCounter>
+                    <CheckoutCardRemoveButton>
+                        <Trash size={16}/>
+                        <p>Remover</p>
+                    </CheckoutCardRemoveButton>
+                </CheckoutCardButtonsContainer>
             </CheckoutCardInfo>
-
+            </CheckoutCardInfoContainer>
+            
+            <CheckoutCardPrice>R$ 9,90</CheckoutCardPrice>
         </CheckoutCardContainer>
     )
 }
