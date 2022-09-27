b7cc9b1a9cd49b74c3a193b238718c26f46f3682
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-27 18:49:22 -0300

    Updated Done page

--- coffeedelivery/src/pages/Done/Index.tsx
+++ coffeedelivery/src/pages/Done/Index.tsx
@@ -1,9 +1,15 @@
-import { DoneContainer } from "./styles";
+import { DoneContainer, DoneInfosReviewContainer, DoneMainContainer, DoneMainText, DoneSubtitleText, } from "./styles";
 
 export function Done(){
     return(
         <DoneContainer>
-            
+            <DoneMainContainer>
+                <DoneMainText> Uhu! Pedido confirmado</DoneMainText>
+                <DoneSubtitleText>Agora é só aguardar que logo o café chegará até você</DoneSubtitleText>
+                <DoneInfosReviewContainer>
+
+                </DoneInfosReviewContainer>
+            </DoneMainContainer>
         </DoneContainer>
     )
 }
